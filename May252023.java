f4580ef3c72e6fe898d453f48ddcb02e20ab9344
Shubham Garg <shubham27dec@gmail.com>
2023-05-25 11:04:33 +0530

    Today's solution uploaded

---
+++ May252023.java
@@ -0,0 +1,34 @@
+public class May252023 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public double new21Game(int n, int k, int maxPts) {
+        if (k == 0 || n >= k + maxPts) {
+            return 1.0;
+        }
+
+        double windowSum = 1.0;
+        double probability = 0.0;
+
+        double[] dp = new double[n + 1];
+        dp[0] = 1.0;
+
+        for (int i = 1; i <= n; i++) {
+            dp[i] = windowSum / maxPts;
+
+            if (i < k) {
+                windowSum += dp[i];
+            } else {
+                probability += dp[i];
+            }
+
+            if (i >= maxPts) {
+                windowSum -= dp[i - maxPts];
+            }
+        }
+
+        return probability;
+    }
+}
