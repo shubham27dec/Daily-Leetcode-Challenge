426026a4a2189a3989418f06b59ed0b8620abab6
Shubham Garg <shubham27dec@gmail.com>
2023-06-14 13:38:51 +0530

    Today's solution uploaded

---
+++ June142023.java
@@ -0,0 +1,45 @@
+public class June142023 {
+
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     *     int val;
+     *     TreeNode left;
+     *     TreeNode right;
+     *     TreeNode() {}
+     *     TreeNode(int val) { this.val = val; }
+     *     TreeNode(int val, TreeNode left, TreeNode right) {
+     *         this.val = val;
+     *         this.left = left;
+     *         this.right = right;
+     *     }
+     * }
+     * */
+
+    class Solution {
+
+        int prev = Integer.MAX_VALUE;
+
+        int ans = Integer.MAX_VALUE;
+        public int getMinimumDifference(TreeNode root) {
+            inOrder(root);
+            return ans;
+        }
+
+        public void inOrder(TreeNode root){
+            if(root.left!=null){
+                inOrder(root.left);
+            }
+            ans = Math.min(ans,Math.abs(root.val-prev));
+            prev = root.val;
+            if(root.right!=null){
+                inOrder(root.right);
+            }
+        }
+    }
+
+}
