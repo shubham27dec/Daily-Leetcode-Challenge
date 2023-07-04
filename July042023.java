a67c1786b241edaf01a05b36b1bce7e087bed4ed
Shubham Garg <shubham27dec@gmail.com>
2023-07-04 22:57:29 +0530

    Today's solution uploaded

---
+++ July042023.java
@@ -0,0 +1,15 @@
+public class July042023 {
+    public static void main(String[] args) {
+
+    }
+    class Solution {
+        public int singleNumber(int[] nums) {
+            int ones = 0, twos = 0;
+            for(int e : nums) {
+                ones = ((ones ^ e) & (~ twos));
+                twos = ((twos ^ e) & (~ ones));
+            }
+            return ones;
+        }
+    }
+}
