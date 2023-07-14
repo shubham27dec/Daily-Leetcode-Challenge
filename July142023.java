1d659744baa0894d61f6d900ab524040ef01a093
Shubham Garg <shubham27dec@gmail.com>
2023-07-14 12:43:40 +0530

    Today's solution uploaded

---
+++ July142023.java
@@ -0,0 +1,16 @@
+public class July142023 {
+    public static void main(String[] args) {
+
+    }
+    class Solution {
+        public int longestSubsequence(int[] arr, int diff) {
+            int [] diffArr = new int[40001];
+            int ans = 1;
+            for(int i : arr){
+                diffArr[i+20000] = diffArr[i-diff+20000]+1;
+                ans = Math.max(ans,diffArr[i+20000]);
+            }
+            return ans;
+        }
+    }
+}
