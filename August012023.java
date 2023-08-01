83b92d44a22972dd4d0bd9c473b3d07995f0b9ee
Shubham Garg <shubham27dec@gmail.com>
2023-08-01 23:30:59 +0530

    Today's solution uploaded

---
+++ August012023.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class August012023 {
+    public static void main(String[] args) {
+        class Solution {
+            List<List<Integer>> ans;
+            public List<List<Integer>> combine(int n, int k) {
+                ans = new ArrayList<>();
+                helper(1,n,k,new ArrayList<>());
+                return ans;
+            }
+
+            public void helper(int start, int n, int k, List<Integer>sub){
+                if(k==0) {
+                    ans.add(new ArrayList<>(sub));
+                    return;
+                }
+                for(int i = start;i<=n-k+1;i++){
+                    sub.add(i);
+                    helper(i+1,n,k-1, sub);
+                    sub.remove(sub.size()-1);
+                }
+            }
+        }
+    }
+}
