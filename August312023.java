d2268de274d0f89325a9e8a24b849fffffbdf1c9
Shubham Garg <shubham27dec@gmail.com>
2023-08-31 15:42:56 +0530

    Today's solution uploaded

---
+++ August312023.java
@@ -0,0 +1,23 @@
+public class August312023 {
+    public static void main(String[] args) {
+
+    }
+    class Solution {
+        public int minTaps(int n, int[] ranges) {
+            int min=0, max=0, count=0;
+            while(max<n){
+                for(int i=0; i<ranges.length; i++){
+                    if(i-ranges[i]<=min && i+ranges[i]>=max){
+                        max = i+ranges[i];
+                    }
+                }
+                if(max==min){
+                    return -1;
+                }
+                count++;
+                min=max;
+            }
+            return count;
+        }
+    }
+}
