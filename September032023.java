849fadd45dbc4d7a0bc31162f30ca1b6a46c667e
Shubham Garg <shubham27dec@gmail.com>
2023-09-03 07:31:11 +0530

    Today's solution uploaded

---
+++ September032023.java
@@ -0,0 +1,21 @@
+public class September032023 {
+    public static void main(String[] args) {
+
+    }
+    class Solution {
+        public int uniquePaths(int m, int n) {
+            int[][] dp = new int[m][n];
+            for(int row=0; row<m; row++){
+                for(int col=0; col<n; col++){
+                    if(row==0 || col==0){
+                        dp[row][col] = 1;
+                    }
+                    else{
+                        dp[row][col] = dp[row-1][col] + dp[row][col-1];
+                    }
+                }
+            }
+            return dp[m-1][n-1];
+        }
+    }
+}
