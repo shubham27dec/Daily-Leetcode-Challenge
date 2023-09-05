234af507583637588ce181c61c2c25c7ec0771aa
Shubham Garg <shubham27dec@gmail.com>
2023-09-05 07:21:54 +0530

    Today's solution uploaded

---
+++ September052023.java
@@ -0,0 +1,29 @@
+import java.util.HashMap;
+
+public class September052023 {
+    public static void main(String[] args) {
+
+    }
+    public class Solution {
+        public Node copyRandomList(Node head) {
+            if (head == null) return null;
+
+            HashMap<Node, Node> oldToNew = new HashMap<>();
+
+            Node curr = head;
+            while (curr != null) {
+                oldToNew.put(curr, new Node(curr.val));
+                curr = curr.next;
+            }
+
+            curr = head;
+            while (curr != null) {
+                oldToNew.get(curr).next = oldToNew.get(curr.next);
+                oldToNew.get(curr).random = oldToNew.get(curr.random);
+                curr = curr.next;
+            }
+
+            return oldToNew.get(head);
+        }
+    }
+}
