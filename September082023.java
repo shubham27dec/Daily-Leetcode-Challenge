ef5cd3f0404f17f8a50e817604915839b6fb4161
Shubham Garg <shubham27dec@gmail.com>
2023-09-08 21:57:11 +0530

    Today's solution uploaded

---
+++ September082023.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class September082023 {
+    public static void main(String[] args) {
+
+    }
+    public class Solution {
+        public List<List<Integer>> generate(int numRows) {
+            List<List<Integer>> triangle = new ArrayList<>();
+            if (numRows == 0) return triangle;
+
+            triangle.add(new ArrayList<>());
+            triangle.get(0).add(1);
+
+            for (int i = 1; i < numRows; i++) {
+                List<Integer> prev_row = triangle.get(i - 1);
+                List<Integer> new_row = new ArrayList<>();
+                new_row.add(1);
+
+                for (int j = 1; j < prev_row.size(); j++) {
+                    new_row.add(prev_row.get(j - 1) + prev_row.get(j));
+                }
+
+                new_row.add(1);
+                triangle.add(new_row);
+            }
+
+            return triangle;
+        }
+    }
+}
