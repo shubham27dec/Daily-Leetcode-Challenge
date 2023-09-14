4ad953837fdac79eb91936d3695afa2aa564fe64
Shubham Garg <shubham27dec@gmail.com>
2023-09-14 12:53:21 +0530

    Today's solution uploaded

---
+++ September142023.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class September142023 {
+    public static void main(String[] args) {
+
+    }
+    class Solution {
+        public List<String> findItinerary(List<List<String>> tickets) {
+            Map<String, PriorityQueue<String>> graph = new HashMap<>();
+
+            for (List<String> ticket : tickets) {
+                graph.putIfAbsent(ticket.get(0), new PriorityQueue<>());
+                graph.get(ticket.get(0)).add(ticket.get(1));
+            }
+
+            LinkedList<String> itinerary = new LinkedList<>();
+
+            dfs("JFK", graph, itinerary);
+
+            return itinerary;
+        }
+
+        private void dfs(String airport, Map<String, PriorityQueue<String>> graph, LinkedList<String> itinerary) {
+            PriorityQueue<String> nextAirports = graph.get(airport);
+            while (nextAirports != null && !nextAirports.isEmpty()) {
+                dfs(nextAirports.poll(), graph, itinerary);
+            }
+            itinerary.addFirst(airport);
+        }
+    }
+}
