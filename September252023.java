207184a4555865ded3aac08e56cdda543db4c470
Shubham Garg <shubham27dec@gmail.com>
2023-09-25 18:44:38 +0530

    Today's solution uploaded

---
+++ September252023.java
@@ -0,0 +1,17 @@
+public class September252023 {
+    public static void main(String[] args) {
+
+    }
+    class Solution {
+        public char findTheDifference(String s, String t) {
+            char c = 0;
+            for(char cs : s.toCharArray()){
+                c ^= cs;
+            }
+            for(char ct : t.toCharArray()){
+                c ^= ct;
+            }
+            return c;
+        }
+    }
+}
