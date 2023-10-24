eeb1525f4d7b5dce38349fe1dd068ef1b5a5c885
Shubham Garg <shubham27dec@gmail.com>
2023-10-24 12:45:39 +0530

    Today's solution uploaded

---
+++ October242023.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class October242023 {
+    public static void main(String[] args) {
+
+    }
+    public class Solution {
+        public List<Integer> largestValues(TreeNode root) {
+            if (root == null) return new ArrayList<>();
+
+            List<Integer> result = new ArrayList<>();
+            Queue<TreeNode> queue = new LinkedList<>();
+            queue.offer(root);
+
+            while (!queue.isEmpty()) {
+                int curr_level_size = queue.size();
+                int max_val = Integer.MIN_VALUE;
+
+                for (int i = 0; i < curr_level_size; i++) {
+                    TreeNode node = queue.poll();
+                    max_val = Math.max(max_val, node.val);
+
+                    if (node.left != null) queue.offer(node.left);
+                    if (node.right != null) queue.offer(node.right);
+                }
+
+                result.add(max_val);
+            }
+
+            return result;
+        }
+    }
+}
