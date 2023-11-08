f72400016a1df1e74de3e2571fdf771603740338
Shubham Garg <shubham27dec@gmail.com>
2023-11-09 04:14:45 +0530

    Today's solution uploaded

---
+++ November082023.java
@@ -0,0 +1,17 @@
+public class November082023 {
+    public static void main(String[] args) {
+
+    }
+    class Solution {
+        public boolean isReachableAtTime(int sx, int sy, int fx, int fy, int t) {
+            int d1=Math.abs(fx-sx);
+            int d2=Math.abs(fy-sy);
+            int d=Math.max(d1,d2);
+            if(d==0&&t==1)
+                return false;
+            if(d<=t)
+                return true;
+            return false;
+        }
+    }
+}
