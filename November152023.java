d51e463dc336ca1e636223bfb773ca7ca9119b8f
Shubham Garg <shubham27dec@gmail.com>
2023-11-16 11:40:31 +0530

    Today's solution uploaded

---
+++ November152023.java
@@ -0,0 +1,24 @@
+public class November152023 {
+    public static void main(String[] args) {
+
+    }
+    class Solution {
+        public int maximumElementAfterDecrementingAndRearranging(int[] arr) {
+            int l = arr.length;
+            int[] counter = new int[l];
+
+            for(int i=0;i<l;i++){
+                ++counter[Math.min(arr[i]-1,l-1)];
+            }
+            int ans = 1;
+            for (int i = 1; i < l; i++){
+                ans = Math.min(i+1, ans + counter[i]);
+            }
+
+            return ans;
+
+
+        }
+
+    }
+}
---
+++ November162023.java
@@ -0,0 +1,14 @@
+public class November162023 {
+    public static void main(String[] args) {
+
+    }
+    class Solution {
+        public String findDifferentBinaryString(String[] nums) {
+            StringBuilder sb = new StringBuilder();
+            for(int i=0; i<nums.length; i++){
+                sb.append(nums[i].charAt(i)=='0'?'1':'0');
+            }
+            return sb.toString();
+        }
+    }
+}
