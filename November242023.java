570050101dc732330c31cad9610660f0d32a1514
Shubham Garg <shubham27dec@gmail.com>
2023-11-24 16:05:00 +0530

    Today's solution uploaded

---
+++ November242023.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class November242023 {
+    public static void main(String[] args) {
+
+    }
+    class Solution {
+        public int maxCoins(int[] piles) {
+            Arrays.sort(piles);
+            int coins = 0;
+            int passes = 0;
+            for(int i=piles.length-2; i>0+passes++;i-=2){
+                coins += piles[i];
+            }
+            return coins;
+        }
+    }
+}
