8b1ef51aa8739f93b95452f1d69c9e897d9329c6
Shubham Garg <shubham27dec@gmail.com>
2023-11-30 10:27:37 +0530

    Today's solution uploaded

---
+++ November302023.java
@@ -0,0 +1,21 @@
+public class November302023 {
+    public static void main(String[] args) {
+
+    }
+    class Solution {
+        public int minimumOneBitOperations(int n) {
+            if(n==0){
+                return 0;
+            }
+
+            int k=0, curr=1;
+
+            while(curr*2 <= n){
+                curr *= 2;
+                k++;
+            }
+
+            return (1<< (k+1)) - 1 - minimumOneBitOperations(n^curr);
+        }
+    }
+}
