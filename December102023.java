38407194e43912569602524f34cbb00dde671504
Shubham Garg <shubham27dec@gmail.com>
2023-12-10 15:12:09 +0530

    Today's solution uploaded

---
+++ December102023.java
@@ -0,0 +1,16 @@
+public class December102023 {
+    public static void main(String[] args) {
+
+    }
+    class Solution {
+        public int[][] transpose(int[][] matrix) {
+            int[][] transpose = new int[matrix[0].length][matrix.length];
+            for(int i=0; i<matrix[0].length; i++){
+                for(int j=0; j<matrix.length; j++){
+                    transpose[i][j] = matrix[j][i];
+                }
+            }
+            return transpose;
+        }
+    }
+}
