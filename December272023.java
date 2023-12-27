5f8048e5e7c686901ecaa1aa90cf41350057ae78
Shubham Garg <shubham27dec@gmail.com>
2023-12-27 23:20:38 +0530

    Today's solution uploaded

---
+++ December272023.java
@@ -0,0 +1,17 @@
+public class December272023 {
+    public static void main(String[] args) {
+
+    }
+    class Solution {
+        public int minCost(String colors, int[] neededTime) {
+            int minTime = 0;
+            for(int i=1; i<colors.length(); i++){
+                if(colors.charAt(i)==colors.charAt(i-1)){
+                    minTime += Math.min(neededTime[i],neededTime[i-1]);
+                    neededTime[i] = Math.max(neededTime[i],neededTime[i-1]);
+                }
+            }
+            return minTime;
+        }
+    }
+}
