9cf264864d68e61bb10917a293c85878b8dbd90f
Shubham Garg <shubham27dec@gmail.com>
2024-02-02 14:13:30 +0530

    Today's solution uploaded

---
+++ Febraury022024.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Febraury022024 {
+    public static void main(String[] args) {
+
+    }
+    class Solution {
+        public static List<Integer> sequentialDigits(int low, int high) {
+            List<Integer> result = new ArrayList<>();
+
+            for (int i = 1; i <= 9; i++) {
+                int num = i;
+
+                for (int j = i + 1; j <= 9; j++) {
+                    num = num * 10 + j;
+
+                    if (num >= low && num <= high) {
+                        result.add(num);
+                    }
+                }
+            }
+
+            Collections.sort(result);
+            return result;
+        }
+    }
+}
