383b39a42ae5427715a371da032581ba4db9c6be
Shubham Garg <shubham27dec@gmail.com>
2024-02-12 21:48:18 +0530

    Today's solution uploaded

---
+++ February122024.java
@@ -0,0 +1,23 @@
+public class February122024 {
+    public static void main(String[] args) {
+
+    }
+    class Solution {
+        public int majorityElement(int[] nums) {
+            int candidate = nums[0];
+            int count = 0;
+            for(int num : nums){
+                if(count==0){
+                    candidate = num;
+                }
+                if(num==candidate){
+                    count++;
+                }
+                else{
+                    count--;
+                }
+            }
+            return candidate;
+        }
+    }
+}
