ae9485b9d65d34571d21ff4005329fb3782ac6af
Shubham Garg <shubham27dec@gmail.com>
2024-02-16 18:02:40 +0530

    Today's solution uploaded

---
+++ February162023.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class February162023 {
+    public static void main(String[] args) {
+
+    }
+    class Solution {
+        public int findLeastNumOfUniqueInts(int[] arr, int k) {
+            Map<Integer,Integer> numFreq = new HashMap<>();
+            for(int num : arr){
+                numFreq.put(num,numFreq.getOrDefault(num,0)+1);
+            }
+            List<Integer> freqs = new ArrayList<>();
+            for(int freq : numFreq.values()){
+                freqs.add(freq);
+            }
+            Collections.sort(freqs);
+            while(true){
+                if(freqs.isEmpty() || freqs.get(0)>k){
+                    break;
+                }
+                else{
+                    k -= freqs.get(0);
+                    freqs.remove(0);
+                }
+            }
+            return freqs.size();
+        }
+    }
+}
