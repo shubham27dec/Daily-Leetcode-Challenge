ae52c4d1d427362bd9826202bc86cd78e005750a
Shubham Garg <shubham27dec@gmail.com>
2024-02-21 10:02:10 +0530

    Today's solution uploaded

---
+++ February212024.java
@@ -0,0 +1,19 @@
+public class February212024 {
+    public static void main(String[] args) {
+
+    }
+    class Solution {
+        public int rangeBitwiseAnd(int left, int right) {
+            int shift = 0;
+
+            while (left < right) {
+                left >>= 1;
+                right >>= 1;
+                shift++;
+            }
+
+            return left << shift;
+
+        }
+    }
+}
