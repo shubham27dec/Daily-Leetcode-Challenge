53e668185c72f2ea585269e328232967d8bded7d
Shubham Garg <shubham27dec@gmail.com>
2024-03-06 00:28:52 +0530

    Today's solution uploaded

---
+++ March052024.java
@@ -0,0 +1,27 @@
+public class March052024 {
+    public static void main(String[] args) {
+
+    }
+    class Solution {
+        public int minimumLength(String s) {
+            int i=0,j=s.length()-1;
+
+            while(i<j && s.charAt(i)==s.charAt(j))
+            {
+                char compare = s.charAt(i);
+
+                while(i<=j && s.charAt(i)==compare)
+                {
+                    i++;
+                }
+
+                while(i<=j && s.charAt(j)==compare)
+                {
+                    j--;
+                }
+
+            }
+            return j-i+1;
+        }
+    }
+}
