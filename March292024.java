c51dbf772bf3a5c7a4c66a77e54ebccf8ec31791
Shubham Garg <shubham27dec@gmail.com>
2024-03-29 20:31:25 +0530

    Today's solution uploaded

---
+++ March292024.java
@@ -0,0 +1,25 @@
+public class March292024 {
+    public static void main(String[] args) {
+
+    }
+    class Solution {
+        public long countSubarrays(int[] nums, int k) {
+            long res = 0;
+            int max = 0, count = 0;
+            for (int num : nums) {
+                max = Math.max(max, num);
+            }
+            int l = 0;
+            for (int r = 0; r < nums.length; r++) {
+                if (nums[r] == max)
+                    count++;
+                while (count >= k) {
+                    if (nums[l++] == max)
+                        count--;
+                }
+                res += l;
+            }
+            return res;
+        }
+    }
+}
