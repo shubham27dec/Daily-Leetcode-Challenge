b6f84e703c554cbc22f4a308331eb2140bba0965
Shubham Garg <shubham27dec@gmail.com>
2024-05-10 21:46:31 +0530

    Today's solution uploaded

---
+++ May092024.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class May092024 {
+    public static void main(String[] args) {
+
+    }
+    class Solution {
+        public long maximumHappinessSum(int[] happiness, int k) {
+            Arrays.sort(happiness);
+            int dec = 0,n=happiness.length;
+            long ans=0;
+            for(int i=n-1;i>=n-k;i--){
+                ans += Math.max(0,happiness[i]-dec);
+                dec++;
+            }
+            return ans;
+        }
+    }
+}
