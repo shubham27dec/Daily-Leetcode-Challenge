a53d5162a9cba440232926f34477ea697f8cf8e1
Shubham Garg <shubham27dec@gmail.com>
2024-05-23 20:55:03 +0530

    Today's solution uploaded

---
+++ May232024.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class May232024 {
+    public static void main(String[] args) {
+
+    }
+    class Solution {
+        private int dfs(int[] nums, int k, int i, Set<Integer> set) {
+            if (i == nums.length)
+                return set.isEmpty() ? 0 : 1;
+
+            int cnt = dfs(nums, k, i + 1, set);
+
+            if (set.contains(nums[i] - k))
+                return cnt;
+
+            set.add(nums[i]);
+            cnt += dfs(nums, k, i + 1, set);
+            set.remove(nums[i]);
+
+            return cnt;
+        }
+
+        public int beautifulSubsets(int[] nums, int k) {
+            Arrays.sort(nums);
+
+            return dfs(nums, k, 0, new HashSet<Integer>());
+        }
+    }
+}
