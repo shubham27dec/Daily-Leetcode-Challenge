20d7b57e7646b9a23778f2ca69d0d5d4af54101f
Shubham Garg <shubham27dec@gmail.com>
2024-06-01 22:31:37 +0530

    Today's solution uploaded

---
+++ June012024.java
@@ -0,0 +1,14 @@
+public class June012024 {
+    public static void main(String[] args) {
+
+    }
+    class Solution {
+        public int scoreOfString(String s) {
+            int score = 0;
+            for(int i=1; i<s.length(); i++){
+                score += Math.abs(s.charAt(i)-s.charAt(i-1));
+            }
+            return score;
+        }
+    }
+}
