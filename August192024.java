263d9f31825140d52c9bd9d202b4e71b211854a3
Shubham Garg <shubham27dec@gmail.com>
2024-08-20 00:40:50 +0530

    Today's solution uploaded

---
+++ August192024.java
@@ -0,0 +1,22 @@
+public class August192024 {
+    public static void main(String[] args) {
+
+    }
+    class Solution {
+
+        public int minSteps(int n) {
+            int ans = 0;
+            int d = 2;
+            while (n > 1) {
+                // If d is prime factor, keep dividing
+                // n by d until is no longer divisible
+                while (n % d == 0) {
+                    ans += d;
+                    n /= d;
+                }
+                d++;
+            }
+            return ans;
+        }
+    }
+}
