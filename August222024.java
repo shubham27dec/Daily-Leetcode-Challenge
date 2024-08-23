5bb3fb782505f8828de0a9f60172d1ffe8338ce9
Shubham Garg <shubham27dec@gmail.com>
2024-08-23 22:48:46 +0530

    Today's solution uploaded

---
+++ August222024.java
@@ -0,0 +1,20 @@
+public class August222024 {
+    public static void main(String[] args) {
+
+    }
+    class Solution {
+        public int findComplement(int num) {
+            String str = Integer.toBinaryString(num);
+            String res = "";
+            for(int i=0;i<str.length();i++){
+                if(str.charAt(i) == '0'){
+                    res += '1';
+                }
+                else{
+                    res += '0';
+                }
+            }
+            return Integer.parseInt(res,2);
+        }
+    }
+}
