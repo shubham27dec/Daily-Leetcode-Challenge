438151f912e739c345ff3936ac9679592fe2955f
Shubham Garg <shubham27dec@gmail.com>
2024-09-19 00:04:50 +0530

    Today's solution uploaded

---
+++ September182024.java
@@ -0,0 +1,19 @@
+public class September182024 {
+    public static void main(String[] args) {
+
+    }
+    class Solution {
+        public String largestNumber(int[] nums) {
+            String[] strings = new String[nums.length];
+            for(int i=0; i<nums.length; i++){
+                strings[i] = String.valueOf(nums[i]);
+            }
+            Arrays.sort(strings,(b,a)->(a+b).compareTo(b+a));
+            StringBuilder sb = new StringBuilder();
+            for(String str : strings){
+                sb.append(str);
+            }
+            return sb.charAt(0)=='0'?"0":sb.toString();
+        }
+    }
+}
