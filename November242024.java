cefe4998c04d4f579599b7968a07395dba40fe38
Shubham Garg <shubham27dec@gmail.com>
2024-11-24 12:10:19 +0530

    Today's solution uploaded

---
+++ November242024.java
@@ -0,0 +1,30 @@
+public class November242024 {
+    public static void main(String[] args) {
+
+    }
+    class Solution {
+
+        public long maxMatrixSum(int[][] matrix) {
+            long totalSum = 0;
+            int minAbsVal = Integer.MAX_VALUE;
+            int negativeCount = 0;
+
+            for (int[] row : matrix) {
+                for (int val : row) {
+                    totalSum += Math.abs(val);
+                    if (val < 0) {
+                        negativeCount++;
+                    }
+                    minAbsVal = Math.min(minAbsVal, Math.abs(val));
+                }
+            }
+
+            // Adjust if the count of negative numbers is odd
+            if (negativeCount % 2 != 0) {
+                totalSum -= 2 * minAbsVal;
+            }
+
+            return totalSum;
+        }
+    }
+}
