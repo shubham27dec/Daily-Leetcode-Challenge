8735baa800d016a74aee15f7febb4c47c22fb92b
Shubham Garg <shubham27dec@gmail.com>
2024-12-18 21:13:14 +0530

    Today's solution uploaded

---
+++ December182024.java
@@ -0,0 +1,28 @@
+import java.util.Stack;
+
+public class December182024 {
+    public static void main(String[] args) {
+
+    }
+    class Solution {
+
+        public int[] finalPrices(int[] prices) {
+            // Create a copy of prices array to store discounted prices
+            int[] result = prices.clone();
+
+            Stack<Integer> stack = new Stack<>();
+
+            for (int i = 0; i < prices.length; i++) {
+                // Process items that can be discounted by current price
+                while (!stack.isEmpty() && prices[stack.peek()] >= prices[i]) {
+                    // Apply discount to previous item using current price
+                    result[stack.pop()] -= prices[i];
+                }
+                // Add current index to stack
+                stack.add(i);
+            }
+
+            return result;
+        }
+    }
+}
